594935dd9c211cce08631ea5dcd561a8acb9dcd2
Kauê Cardoso <54649009+Smutsia14@users.noreply.github.com>
2019-11-09 01:10:26 -0300

    Add files via upload

---
+++ PythonMotivos.py
@@ -0,0 +1,9 @@
+print("Seja Bem vindo !!! \nVoçê está no maior motivador para empregos de Kauês Cardosos do mundo.")
+print("Voçê verá um dos motivos para contrata-lo \nVoçê está pronto ?")
+input ("Pressione Enter para continuar")
+i = 1
+while i <= 1000000:
+    print(f"Ele sabe imprimir todos esse números na tela: {i}")
+    i = i + 1
+
+    
